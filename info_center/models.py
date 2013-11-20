5cd17038ceb7c186ec71d97f050be2cc011d369d
wind1900 <weishen1900@gmail.com>
2013-11-20 09:48:01 +0000

    add fetion main panel

--- info_center/models.py
+++ info_center/models.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-
+
 from django.db import models
 
 class Category(models.Model):
@@ -18,16 +20,30 @@ class Event(models.Model):
     title = models.CharField(max_length=40)
     content = models.TextField()
     date = models.DateField()
-    attach = models.CharField(max_length=30)
+    attach = models.CharField(max_length=30, blank=True)
     # event important level
     # level 1: very important
     level = models.IntegerField()
     def __unicode__(self):
         return u"{0} {1}".format(self.title, self.date)
+    class Meta:
+        ordering = ('-date',)
 
 class Memday(models.Model):
     # for the nth m day in a month, use n*100+m to represent
+    # Monday == 1 ... Sunday == 7
     name = models.CharField(max_length=20)
     month = models.IntegerField()
     day = models.IntegerField()
     lunar = models.BooleanField()
+    def __unicode__(self):
+        return self.name
+
+class Birthday(models.Model):
+    name = models.CharField(max_length=10)
+    phone = models.CharField(max_length=11)
+    month = models.IntegerField()
+    day = models.IntegerField()
+    lunar = models.BooleanField()
+    def __unicode__(self):
+        return u"{0}({1})的生日".format(self.name, self.phone)
--- info_center/views.py
+++ info_center/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.db.models import Q
 import importlib
 import models
+import datetime
+import lunardate
 
 def index(request):
     template = loader.get_template('index.html')
@@ -13,10 +16,34 @@ def index(request):
     return HttpResponse(template.render(context))
 
 def fetion(request):
+    today = datetime.date.today()
+    todaya1 = today + datetime.timedelta(days=1)
+    todaya7 = today + datetime.timedelta(days=7)
+    todayb7 = today - datetime.timedelta(days=7)
+    future = models.Event.objects.filter(date__gt=todaya1, 
+                                         date__lte=todaya7)
+    tomorrow = models.Event.objects.filter(date=todaya1)
+    now = models.Event.objects.filter(date = today)
+    past = models.Event.objects.filter(date__gte=todayb7,
+                                       date__lt=today)
+    lunar_today = lunardate.LunarDate.fromSolarDate(today.year, today.month, today.day)
+    lunar_todaya1 = lunardate.LunarDate.fromSolarDate(todaya1.year, todaya1.month, todaya1.day)
+    mthn = 100 * (today.day//7 + 1) + today.weekday() + 1
+    mthn1 = 100 * (todaya1.day//7 + 1) + todaya1.weekday() + 1
+    memdays = models.Memday.objects.filter(Q(month=today.month, day = today.day, lunar = False) | Q(month = lunar_today.month, day = lunar_today.day, lunar = True) | Q(month = today.month, day = mthn, lunar = False))
+    tomorrow_memdays = models.Memday.objects.filter(Q(month=todaya1.month, day = todaya1.day, lunar = False) | Q(month = lunar_todaya1.month, day = lunar_todaya1.day, lunar=True) | Q(month = today.month, day = mthn1, lunar = False))
+    memdays = " ".join(e.name for e in memdays)
+    tomorrow_memdays = " ".join(e.name for e in tomorrow_memdays)
     template = loader.get_template('fetion.html')
     context = RequestContext(request,
                              {'title':'fetion',
-                              'active_navbar':'fetion'})
+                              'active_navbar':'fetion',
+                              'future':future,
+                              'tomorrow':tomorrow,
+                              'now':now,
+                              'past':past,
+                              'now_memdays':memdays,
+                              'tomorrow_memdays':tomorrow_memdays})
     return HttpResponse(template.render(context))
 
 def update(request, source):
